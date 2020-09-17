6bbce74bb11d38ca4691960de45e5e9d2af5f454
Octavio Marchi <octaviomarchi@msn.com>
2020-09-17 18:25:42 -0300

    spring boot init
    spring boot initialized for standalone project

---
+++ src/test/java/octavio/challenges/floodedsilhouettes/FloodedSilhouettesApplicationTests.java
@@ -0,0 +1,13 @@
+package octavio.challenges.floodedsilhouettes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloodedSilhouettesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
